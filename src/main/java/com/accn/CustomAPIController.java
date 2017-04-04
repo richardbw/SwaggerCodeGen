23270729f5b1f879e2a74a476a0f38019dd571ac
U-DIR\richard.barnes-webb <richard.barnes-webb@CPX-KN7SY4MHOO6.dir.svc.accenture.com>
2017-04-04 16:20:06 +0100

    oops!

--- src/main/java/com/accn/CustomAPIController.java
+++ src/main/java/com/accn/CustomAPIController.java
@@ -2,28 +2,37 @@ package com.accn;
 
 import com.accn.swagger.api.AtmsApiController;
 import com.accn.swagger.model.OpenBankingInlineResponse2001;
+import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by richard.barnes-webb on 27/03/2017.
  *
  */
 @Controller
-public class CustomAPIController  {
+public class CustomAPIController extends AtmsApiController {
     private static final Logger log = LoggerFactory.getLogger(CustomAPIController.class);
 
-    @RequestMapping(value = "/atms", params = { "type=42" },
-            produces = { "application/prs.openbanking.opendata.v1.3+json" },
+    @RequestMapping(value = "/atmsXXX", 
             method = RequestMethod.GET)
     public ResponseEntity<OpenBankingInlineResponse2001> atmsGet(String ifModifiedSince, String ifNoneMatch) {
         log.info("............XXXXXXXXXXXXXXXX...............................");
         return new ResponseEntity<OpenBankingInlineResponse2001>(HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/atmsXXX", 
+        method = RequestMethod.HEAD)
+
+    public ResponseEntity<Object> atmsHead(@ApiParam(value = "Used for conditional request, to retrieve data only if modified since a given date"  ) @RequestHeader(value="If-Modified-Since", required=false) String ifModifiedSince,
+                                           @ApiParam(value = "Used for conditional request, to retrieve data only if the given Etag value does not match"  ) @RequestHeader(value="If-None-Match", required=false) String ifNoneMatch) {
+        log.info("............YYYYYYYYYYYYYYYY...............................");
+        return new ResponseEntity<Object>(HttpStatus.OK);
     }
 
 }
